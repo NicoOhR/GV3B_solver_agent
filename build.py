bd8c587463f80565ccedb1c17036cbeca7587a86
NicoOhR <nimoohayonr@gmail.com>
2024-11-18 21:31:25 -0600

    working protobuf fetcher

--- build.py
+++ build.py
@@ -1,11 +1,12 @@
 from pybuilder.core import use_plugin, init, task
 import requests
+import os
 
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 
-default_task = ["get_protobuf_service_def", "publish"]
+default_task = ["publish"]
 
 
 @init
@@ -14,15 +15,19 @@ def set_properties(project):
     project.build_depends_on("grpcio")
     project.build_depends_on("grpcio-tools")
     project.build_depends_on("protobuf")
+    get_protobuf_service_def(project)
 
 
-@task
 def get_protobuf_service_def(project):
-    protobuf_url = "https://github.com/NicoOhR/GV3B_simulation/blob/main/proto/simulation.protobuf_url"
+    protobuf_url = "https://raw.githubusercontent.com/NicoOhR/GV3B_simulation/refs/heads/main/proto/simulation.proto"
     response = requests.get(protobuf_url)
     if response.status_code == 200:
-        with open("simulation.proto", "wb") as file:
+        proto_file = os.path.join(project.basedir, "proto/simulation.proto")
+        proto_dir = os.path.join(project.basedir, "proto/")
+        os.makedirs(proto_dir, exist_ok=True)
+        with open(proto_file, "wb") as file:
             file.write(response.content)
-            print("Successfuly got latest protobuf")
+            print("[Builder] Successfuly got latest protobuf")
     else:
-        print("Failed to get protobuf")
+        print("[Builder] Failed to get protobuf")
+        print(response.status_code)
