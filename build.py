9224050aeae04bd26aee007c24d79e6cba1fef15
NicoOhR <nimoohayonr@gmail.com>
2024-11-19 00:22:46 -0600

    working protobuf compiling

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 from pybuilder.core import use_plugin, init, task
+import subprocess
 import requests
 import os
 
@@ -6,7 +7,7 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 
-default_task = ["get_protobuf_service_def", "publish"]
+default_task = ["get_protobuf_service_def", "build_proto_buf", "publish"]
 
 
 @init
@@ -24,10 +25,46 @@ def get_protobuf_service_def(project):
     if response.status_code == 200:
         proto_dir = os.path.join(project.basedir, "src/main/proto/")
         proto_file = os.path.join(proto_dir, "simulation.proto")
+        generated_dir = os.path.join(
+            project.basedir, project.get_property("dir_source_main_python"), "generated"
+        )
+        project.set_property("proto_dir", proto_dir)
+        project.set_property("proto_file", proto_file)
+        project.set_property("proto_output_path", generated_dir)
         os.makedirs(proto_dir, exist_ok=True)
+        os.makedirs(generated_dir, exist_ok=True)
         with open(proto_file, "wb") as file:
             file.write(response.content)
             print("[Builder] Successfuly got latest protobuf")
     else:
         print("[Builder] Failed to get protobuf")
         print(response.status_code)
+
+
+@task
+def build_proto_buf(project):
+    proto_file = project.get_property("proto_file")
+    proto_dir = project.get_property("proto_dir")
+    proto_output_path = project.get_property("proto_output_path")
+    command = [
+        "python3",
+        "-m",
+        "grpc_tools.protoc",
+        "-I",
+        proto_dir,
+        f"--python_out={proto_output_path}",
+        f"--grpc_python_out={proto_output_path}",
+        proto_file,
+    ]
+
+    try:
+        result = subprocess.run(command, text=True, check=True)
+        print("[Builder] Protobuf compilation completed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"[Builder] Protobuf compilation failed with error code {e.returncode}")
+    except FileNotFoundError:
+        print(
+            "[Builder] Python3 or grpc_tools not found. Ensure they are installed and available in PATH."
+        )
+    except Exception as e:
+        print(f"[Builder] An unexpected error occurred: {e}")
