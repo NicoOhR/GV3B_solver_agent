6cc4729f40b3ad5425f566fff074063a342d57d2
NicoOhR <nimoohayonr@gmail.com>
2024-11-18 22:05:26 -0600

    moving protobuf

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 
-default_task = ["publish"]
+default_task = ["get_protobuf_service_def", "publish"]
 
 
 @init
@@ -15,15 +15,15 @@ def set_properties(project):
     project.build_depends_on("grpcio")
     project.build_depends_on("grpcio-tools")
     project.build_depends_on("protobuf")
-    get_protobuf_service_def(project)
 
 
+@task
 def get_protobuf_service_def(project):
     protobuf_url = "https://raw.githubusercontent.com/NicoOhR/GV3B_simulation/refs/heads/main/proto/simulation.proto"
     response = requests.get(protobuf_url)
     if response.status_code == 200:
-        proto_file = os.path.join(project.basedir, "proto/simulation.proto")
-        proto_dir = os.path.join(project.basedir, "proto/")
+        proto_dir = os.path.join(project.basedir, "src/main/proto/")
+        proto_file = os.path.join(proto_dir, "simulation.proto")
         os.makedirs(proto_dir, exist_ok=True)
         with open(proto_file, "wb") as file:
             file.write(response.content)
